88351a2919a859d85cf9f5ef4e6373916013309b
Fady Adal <2masadel@gmail.com>
2020-10-12 16:25:23 +0200

    some flesh (sorry)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,32 @@
  */
 
 module.exports = {
-  plugins: ["gatsby-plugin-netlify-cms"],
+  siteMetadata: {
+    title: "The STEM Gazette",
+    author: {
+      name: "The STEM October Team",
+      summary: "A group of students from STEM October",
+    },
+    description: `All the news happening within the STEM October campus.`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "blog",
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
