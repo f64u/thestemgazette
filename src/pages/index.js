a4ab0ee79e5e92451a73c5cee4f6571527c6b6e1
Fady Adal <2masadel@gmail.com>
2020-12-01 20:01:42 +0200

    added home title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import React from "react";
 import Layout from "../components/common/layout";
 import SEO from "../components/common/seo";
 
+import config from "../../data/SiteConfig";
+import { Helmet } from "react-helmet";
+
 const Home = () => {
   return (
     <Layout>
+      <Helmet title={`Home | ${config.siteTitle}`} />
       <SEO />
       <div>Hello world!</div>
     </Layout>
